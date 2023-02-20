da4828e81b12838ff13d227c615dfde4ed7143e4
Kyle <k.james.close@gmail.com>
2023-02-20 15:53:56 -0800

    looking decent. 2 more tabs to complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 import { pageLoad } from "./page-load";
+import { displayHomePageContent } from "./home";
 
 pageLoad();
+displayHomePageContent();
+const buttons = document.querySelectorAll("button");
+
+[...buttons].forEach((button) => {
+  button.addEventListener("click", (e) => {
+    // Delete child node, then create/display new node
+    deleteChild();
+    switch (e.target.parentElement.className) {
+      case "home":
+        console.log("Clicked home button");
+        displayHomePageContent();
+        break;
+      case "menu":
+        console.log("Clicked menu button");
+        break;
+      case "contact":
+        console.log("Clicked contact button");
+        break;
+    }
+  });
+});
+
+function deleteChild() {
+  const contentSection = document.querySelector(".content");
+  if (contentSection.firstChild != null) {
+    contentSection.removeChild(contentSection.firstChild);
+  }
+}
