626fa91b560cb9dd0209f764f68f949c3a61dfbe
Kyle <k.james.close@gmail.com>
2023-02-20 21:00:55 -0800

    donzo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import { pageLoad } from "./page-load";
 import { displayHomePageContent } from "./home";
+import { displayMenuContent } from "./menu";
+import { displayContactContent } from "./contact";
 
 pageLoad();
 displayHomePageContent();
@@ -7,18 +9,18 @@ const buttons = document.querySelectorAll("button");
 
 [...buttons].forEach((button) => {
   button.addEventListener("click", (e) => {
+    console.log("test");
     // Delete child node, then create/display new node
     deleteChild();
     switch (e.target.parentElement.className) {
       case "home":
-        console.log("Clicked home button");
         displayHomePageContent();
         break;
       case "menu":
-        console.log("Clicked menu button");
+        displayMenuContent();
         break;
       case "contact":
-        console.log("Clicked contact button");
+        displayContactContent();
         break;
     }
   });
@@ -28,5 +30,6 @@ function deleteChild() {
   const contentSection = document.querySelector(".content");
   if (contentSection.firstChild != null) {
     contentSection.removeChild(contentSection.firstChild);
+    contentSection.classList.remove(contentSection.classList[1]);
   }
 }
